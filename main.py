6ef4d7c7ee8da554784c168083bd1d765c39f20c
igor0006 <igordubrovskij69@gmail.com>
2024-10-09 20:10:44 +0300

    remove approximation in calculations

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def input_data():
 def find_pivot(col_pivot_index, A, b, precision):
     ratio = [0] * len(A)
     for i in range(len(A)):
-        ratio[i] = Decimal(str(b[i + 1] / A[i][col_pivot_index])).quantize(Decimal(precision), ROUND_HALF_UP)
+        ratio[i] = Decimal(str(b[i + 1] / A[i][col_pivot_index]))
     m = max(ratio)
     if m <= 0:
         return -1, -1
@@ -46,9 +46,9 @@ def find_pivot(col_pivot_index, A, b, precision):
 
 def transform(C, A, b, row_pivot_index, col_pivot_index, pivot, precision):
 
-    b[row_pivot_index + 1] = Decimal(str(b[row_pivot_index + 1] / pivot)).quantize(Decimal(precision), ROUND_HALF_UP)
+    b[row_pivot_index + 1] = Decimal(str(b[row_pivot_index + 1] / pivot))
     for i in range(len(A[row_pivot_index])):
-        A[row_pivot_index][i] = Decimal(str(A[row_pivot_index][i] / pivot)).quantize(Decimal(precision), ROUND_HALF_UP)
+        A[row_pivot_index][i] = Decimal(str(A[row_pivot_index][i] / pivot))
         
     for i in range(len(A)):
         if i == row_pivot_index:
@@ -56,15 +56,13 @@ def transform(C, A, b, row_pivot_index, col_pivot_index, pivot, precision):
         coefficient = A[i][col_pivot_index]
         b[i + 1] = b[i + 1] - coefficient * b[row_pivot_index + 1]
         for j in range(len(A[i])):
-            A[i][j] = (Decimal(str(A[i][j] - A[row_pivot_index][j] * coefficient))
-                       .quantize(Decimal(precision), ROUND_HALF_UP))
+            A[i][j] = (Decimal(str(A[i][j] - A[row_pivot_index][j] * coefficient)))
 
     coefficient = C[col_pivot_index]
     b[0] = (Decimal(str(b[0] - b[row_pivot_index + 1] * coefficient))
             .quantize(Decimal(precision), ROUND_HALF_UP))
     for i in range(len(C)):
-        C[i] = (Decimal(str(C[i] - A[row_pivot_index][i] * coefficient))
-                .quantize(Decimal(precision), ROUND_HALF_UP))
+        C[i] = (Decimal(str(C[i] - A[row_pivot_index][i] * coefficient)))
     return C, A, b
 
 
@@ -79,7 +77,7 @@ def solve():
         C, A, b = transform(C, A, b, row_pivot_index, col_pivot_index, pivot, precision)
     if problem == "max":
         print("Vector of coefficients of objective function: ")
-        print(C)
+        print([float(Decimal(x).quantize(Decimal(precision))) for x in C])
         print("\n")
         print("Maximum value of objective function: ")
         print(b[0])
@@ -87,7 +85,7 @@ def solve():
         print("Vector of coefficients of objective function: ")
         for i in range(len(C)):
             C[i] = -C[i]
-        print(C)
+        print([float(Decimal(x).quantize(Decimal(precision))) for x in C])
         print("\n")
         print("Minimum value of objective function: ")
         print(-b[0])
