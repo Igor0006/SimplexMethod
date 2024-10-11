9d2e567e8a5f454ceb01ee0b4da74e843b9fad74
Sergey Knyazkin <omgsomuchhomework@gmail.com>
2024-10-11 16:56:35 +0300

    Changed approximation accuracy, clean code

--- main.py
+++ main.py
@@ -30,20 +30,22 @@ def input_data():
     b += list(map(int, input("Enter the vector of right-hand side numbers\n").split()))
     # Vector of right-hand side numbers
 
-    accuracy = int(input("Enter the approximation accuracy\n"))
+    accuracy = float(input("Enter the approximation accuracy\n"))
     # Approximation accuracy
-    precision = "1." + "0" * accuracy
 
-    return C, A, b, problem, variables_num, precision
+    precision = "1." + "0" * int(input("Enter the precision accuracy "
+                                       "(how many numbers after the decimal point)\n"))
+
+    return C, A, b, problem, variables_num, accuracy, precision
 
 
-def find_pivot(col_pivot_index, A, b, precision):
+def find_pivot(col_pivot_index, A, b):
     ratio = [0] * len(A)
     for i in range(len(A)):
         if A[i][col_pivot_index] == 0:
             ratio[i] = -1
             continue
-        ratio[i] = Decimal(str(b[i + 1] / A[i][col_pivot_index]))
+        ratio[i] = b[i + 1] / A[i][col_pivot_index]
     m = max(ratio)
     if m <= 0:
         return -1, -1
@@ -55,10 +57,10 @@ def find_pivot(col_pivot_index, A, b, precision):
     return pivot_element, row_pivot_index
 
 
-def transform(C, A, b, row_pivot_index, col_pivot_index, pivot, precision):
-    b[row_pivot_index + 1] = Decimal(str(b[row_pivot_index + 1] / pivot))
+def transform(C, A, b, row_pivot_index, col_pivot_index, pivot):
+    b[row_pivot_index + 1] = b[row_pivot_index + 1] / pivot
     for i in range(len(A[row_pivot_index])):
-        A[row_pivot_index][i] = Decimal(str(A[row_pivot_index][i] / pivot))
+        A[row_pivot_index][i] = A[row_pivot_index][i] / pivot
 
     for i in range(len(A)):
         if i == row_pivot_index:
@@ -66,13 +68,12 @@ def transform(C, A, b, row_pivot_index, col_pivot_index, pivot, precision):
         coefficient = A[i][col_pivot_index]
         b[i + 1] = b[i + 1] - coefficient * b[row_pivot_index + 1]
         for j in range(len(A[i])):
-            A[i][j] = (Decimal(str(A[i][j] - A[row_pivot_index][j] * coefficient)))
+            A[i][j] = A[i][j] - A[row_pivot_index][j] * coefficient
 
     coefficient = C[col_pivot_index]
-    b[0] = (Decimal(str(b[0] - b[row_pivot_index + 1] * coefficient))
-            .quantize(Decimal(precision), ROUND_HALF_UP))
+    b[0] = b[0] - b[row_pivot_index + 1] * coefficient
     for i in range(len(C)):
-        C[i] = (Decimal(str(C[i] - A[row_pivot_index][i] * coefficient)))
+        C[i] = C[i] - A[row_pivot_index][i] * coefficient
     return C, A, b
 
 
@@ -96,14 +97,22 @@ def find_decision_variables(C, A, b, variables_num):
 
 
 def solve():
-    C, A, b, problem, variables_num, precision = input_data()
+    C, A, b, problem, variables_num, accuracy, precision = input_data()
     while min(C) < 0:
+        C_previous = C.copy()
+        A_previous = A.copy()
+        b_previous = b.copy()
         col_pivot_index = C.index(min(C))
-        pivot, row_pivot_index = find_pivot(col_pivot_index, A, b, precision)
+        pivot, row_pivot_index = find_pivot(col_pivot_index, A, b)
         if pivot == -1:
             print("The method is not applicable!")
             return
-        C, A, b = transform(C, A, b, row_pivot_index, col_pivot_index, pivot, precision)
+        C, A, b = transform(C, A, b, row_pivot_index, col_pivot_index, pivot)
+        if abs(b_previous[0] - b[0]) < accuracy:
+            C = C_previous
+            A = A_previous
+            b = b_previous
+            break
     if problem == "max":
         print("Vector of decision variables: ")
         decision_variables = find_decision_variables(C, A, b, variables_num)
@@ -113,7 +122,7 @@ def solve():
     elif problem == "min":
         print("Vector of decision variables: ")
         decision_variables = find_decision_variables(C, A, b, variables_num)
-        print([float(Decimal(x).quantize(Decimal(precision))) for x in decision_variables])
+        print([float(Decimal(x).quantize(Decimal(precision), ROUND_HALF_UP)) for x in decision_variables])
         print("Minimum value of objective function: ")
         print(-b[0])
 
